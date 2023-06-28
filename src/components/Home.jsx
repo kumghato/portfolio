c9574d32cb09792a861e7a9dc4ac71eb3f56b91d
Kumghato khala <kumghatokhala11@gmail.com>
2023-06-28 11:05:51 +0530

    added resume link

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ function Home() {
                         <h1 className='fs-1'>I'm</h1>
                         <h1 className='name fw-bold my-3'>Kumghato K Khala</h1>
                         <h1 className='mb-5 fs-2'>Fullstack Web Developer</h1>
-                        <a href='https://www.google.com' target='_blank' className='btn_custom px-5 py-2 text-center d-flex align-items-center justify-content-center'>Resume</a>
+                        <a href='https://drive.google.com/file/d/1va2OZlEurPsi9XuwWKq8wb6AB_b5QfEL/view?usp=drive_link' target='_blank' className='btn_custom px-5 py-2 text-center d-flex align-items-center justify-content-center'>Resume</a>
 
                     </div>
                     <div className='col-6 col-md-6 col-sm-12 d-flex justify-content-center'>
